e64d93cded68114951b907153230be584845b0ca
Matt Duffy <m.duffy12610@gmail.com>
2015-01-20 11:32:26 -0400

    Initial commit.

---
+++ Arduino_Style_Guide/Arduino_Style_Guide.ino
@@ -0,0 +1,10 @@
+/*
+    This is a multi-line comment, also called a block comment.
+    Its start is demarked by the token '/*' and it will end with the token:
+ */
+    
+/*
+    The computer ignores everything you put in commnets.  
+    Comments are there for people to read, not machines.
+ */
+    
---
+++ Arduino_Style_Guide/Arduino_Style_Guide/Arduino_Style_Guide.ino
@@ -0,0 +1,23 @@
+/*
+    This is a multi-line comment, also called a block comment.
+    Its start is demarked by the token '/*' and it will end with the token:
+ */
+    
+/*
+    This is a new block comment.
+    The computer read the block-comment end token on line 4 so
+    we had to start a new one.
+    The computer ignores everything you put in comments.  
+    Comments are there for people to read, not machines.
+ */
+ 
+ // This is a single-line comment, or inline comment.
+ // An inline comment starts at the token '//' and goes to the end of that line.
+
+/*
+    This file is a style guide for 'Interactive Computing.'
+    It will have a short example of the concepts we will use in our programs.
+    It is not intedended to be a definitive reference,
+    It covers the basics: just enough to be dangerous.
+    */
+    
